62fdf3fd5da5562755efcde8ef3d15d9b9971f5e
chloeskt <chloe.sekkat@ensae.fr>
2020-12-19 15:29:14 +0100

    add left paddle user control

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ let canvas;
 let canvasContext;
 let ballX = 50;
 let ballSpeedX = 10;
+let ballY = 50;
+let ballSpeedY = 4;
+
+let paddle1Y = 250;
+const PADDLE_HEIGHT = 100;
 
 function moveElements() {
   ballX += ballSpeedX;
@@ -11,6 +16,25 @@ function moveElements() {
   if (ballX < 0) {
     ballSpeedX = -ballSpeedX;
   }
+
+  ballY += ballSpeedY;
+  if (ballY > canvas.height) {
+    ballSpeedY = -ballSpeedY;
+  }
+  if (ballY < 0) {
+    ballSpeedY = -ballSpeedY;
+  }
+}
+
+function calculateMousePosition(event) {
+  const rect = canvas.getBoundingClientRect();
+  const root = document.documentElement;
+  const mouseX = event.clientX - rect.left - root.scrollLeft;
+  const mouseY = event.clientY - rect.top - root.scrollTop;
+  return {
+    x: mouseX,
+    y: mouseY,
+  };
 }
 
 function drawRectangle(leftX, topY, width, height, drawColor) {
@@ -27,9 +51,9 @@ function drawBall(centerX, centerY, radius, drawColor) {
 
 function drawGame() {
   drawRectangle(0, 0, canvas.width, canvas.height, 'black');
-  drawRectangle(0, 200, 10, 100, 'white');
+  drawRectangle(0, paddle1Y, 10, PADDLE_HEIGHT, 'white');
   drawRectangle(800, 200, -10, 100, 'white');
-  drawBall(ballX, 50, 10, 'white');
+  drawBall(ballX, ballY, 10, 'white');
 }
 
 window.onload = () => {
@@ -43,5 +67,14 @@ window.onload = () => {
       drawGame();
     },
     1000 / framesPerSecond,
+  );
+
+  canvas.addEventListener(
+    'mousemove',
+    (event) => {
+      const mousePosition = calculateMousePosition(event);
+      paddle1Y = mousePosition.y - (PADDLE_HEIGHT / 2);
+    },
+
   );
 };
