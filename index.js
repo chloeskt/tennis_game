5ec02f15e16b17610a807fadf2cc356f02255730
chloeskt <chloe.sekkat@ensae.fr>
2020-12-19 15:01:06 +0100

    fix lint

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-var canvas;
-var canvasContext;
-var ballX = 50;
-var ballSpeedX = 10;
-
-window.onload = function() {
-    canvas = document.getElementById("gameCanvas");
-    canvasContext = canvas.getContext("2d");
-
-    var framesPerSecond = 30;
-    setInterval(
-        function() {
-            moveElements();
-            drawGame();
-        },
-        1000/framesPerSecond
-    );
-}
+let canvas;
+let canvasContext;
+let ballX = 50;
+let ballSpeedX = 10;
 
 function moveElements() {
-    ballX = ballX + ballSpeedX;
-    if(ballX > canvas.width) {
-        ballSpeedX = -ballSpeedX;
-    }
-    if(ballX < 0) {
-        ballSpeedX = -ballSpeedX;
-    }
-}
-
-function drawGame() {
-    colorRectangle(0, 0, canvas.width, canvas.height, "black");
-    colorRectangle(0, 200, 10, 100, "white");
-    colorRectangle(800, 200, -10, 100, "white");
-    colorRectangle(ballX, 100, 10, 10 ,"red");
+  ballX += ballSpeedX;
+  if (ballX > canvas.width) {
+    ballSpeedX = -ballSpeedX;
+  }
+  if (ballX < 0) {
+    ballSpeedX = -ballSpeedX;
+  }
 }
 
 function colorRectangle(leftX, topY, width, height, drawColor) {
-    canvasContext.fillStyle = drawColor;
-    canvasContext.fillRect(leftX, topY, width, height);
+  canvasContext.fillStyle = drawColor;
+  canvasContext.fillRect(leftX, topY, width, height);
 }
+
+function drawGame() {
+  colorRectangle(0, 0, canvas.width, canvas.height, 'black');
+  colorRectangle(0, 200, 10, 100, 'white');
+  colorRectangle(800, 200, -10, 100, 'white');
+  colorRectangle(ballX, 100, 10, 10, 'red');
+}
+
+window.onload = () => {
+  canvas = document.getElementById('gameCanvas');
+  canvasContext = canvas.getContext('2d');
+
+  const framesPerSecond = 30;
+  setInterval(
+    () => {
+      moveElements();
+      drawGame();
+    },
+    1000 / framesPerSecond,
+  );
+};
